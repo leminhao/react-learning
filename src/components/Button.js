f54b1ae2acd9ee6b83079b2ddfa1efd6b5a54e5d
Le Minh Hao <haobarcacules@gmail.com>
2021-05-10 08:55:04 +0700

    reused component button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,9 @@ import "../assets/css/cart.css";
 class Button extends React.Component {
   render() {
     return (
-      <div>
-        <button onClick={this.props.value} className={`"${this.props.name}"`}>{this.props.children}</button>
-      </div>
+      <React.Fragment>
+        <button onClick={this.props.value} className={this.props.name}>{this.props.children}</button>
+      </React.Fragment>
     );
   }
 }
